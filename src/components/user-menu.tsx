e795dfbe629804cfa63a53854488345401cc9350
Aditya Kumar <adityakumar13043@gmail.com>
2026-04-02 01:14:47 +0530

    Redirect home to chat and improve mobile user menu alignment

--- src/components/user-menu.tsx
+++ src/components/user-menu.tsx
@@ -94,9 +94,9 @@ export const UserMenu = () => {
             </SidebarMenuButton>
           </DropdownMenuTrigger>
           <DropdownMenuContent
-            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+            className="max-sm:w-64"
             side={isMobile ? 'bottom' : 'right'}
-            align="end"
+            align={isMobile ? 'start' : 'end'}
             sideOffset={4}
           >
             <DropdownMenuLabel className="p-0 font-normal">
